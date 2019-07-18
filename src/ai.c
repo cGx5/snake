c73813085703c852abfe1de0314438d07d6209b4
cGx5 <cGx5@cGx5.com>
2019-07-18 10:38:00 +0800

    update ai

--- src/ai.c
+++ src/ai.c
@@ -1,9 +1,68 @@
 #include "ai.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 const static int dx[] = {0, 1, 0, -1};
 const static int dy[] = {1, 0, -1, 0};
+const int N = 100;
+const int M = 100000;
+
+void bfs(int sx, int sy, game_t* game, int dis[][N]) {
+  static int qx[M], qy[M];
+  int head = 0, tail = 0;
+  memset(dis, 0x3f3f3f3f, sizeof(int) * N * N);
+  dis[sx][sy] = 0;
+  qx[head] = sx, qy[head] = sy, head++;
+  while (tail < head) {
+    int x = qx[tail], y = qy[tail];
+    tail++;
+    for (int i = 0; i < 4; i++) {
+      int tx = x + dx[i], ty = y + dy[i];
+      if (*board_cell(game->board, tx, ty) == CELL_OPEN &&
+          dis[tx][ty] > dis[x][y] + 1) {
+        dis[tx][ty] = dis[x][y] + 1;
+        qx[head] = tx, qy[head] = ty, head++;
+      }
+    }
+  }
+}
+
+int eval(game_t* game, int sx, int sy, snake_t* snake2) {
+  int dis1[N][N], dis2[N][N];
+  bfs(sx, sy, game, dis1);
+  bfs(snake2->head->row, snake2->head->col, game, dis2);
+  int cnt1 = 0, cnt2 = 0;
+  for (int i = 0; i < game->board->rows; i++) {
+    for (int j = 0; j < game->board->cols; j++) {
+      cnt1 += dis1[i][j] < dis2[i][j];
+      cnt2 += dis2[i][j] < dis1[i][j];
+    }
+  }
+  return cnt1 - cnt2 + 10000;
+}
+
+direction_t simple(game_t *game, snake_t *snake1, snake_t *snake2) {
+  int x = snake1->head->row, y = snake1->head->col;
+  direction_t cand[10];
+  int pd = 0;
+  int mx_score = 0;
+  for (int i = 0; i < 4; i++) {
+    int tx = x + dx[i], ty = y + dy[i];
+    if (*board_cell(game->board, tx, ty) == CELL_OPEN) {
+      int score = eval(game, tx, ty, snake2);
+      if (score > mx_score) {
+        mx_score = score;
+        cand[0] = i;
+        pd = 1;
+      } else if (score == mx_score) {
+        cand[pd++] = i;
+      }
+    }
+  }
+  if (pd == 0) return NORTH;
+  return cand[rand() % pd];
+}
 
 direction_t rand_action(game_t *game, snake_t *snake) {
   int x = snake->head->row;
@@ -19,9 +78,10 @@ direction_t rand_action(game_t *game, snake_t *snake) {
 }
 
 direction_t get_action1(game_t *game) {
-  return rand_action(game, game->snake1);
+  return simple(game, game->snake1, game->snake2);
+  //  return rand_action(game, game->snake1);
 }
 
 direction_t get_action2(game_t *game) {
-  return rand_action(game, game->snake2);
+  return simple(game, game->snake2, game->snake1);
 }
